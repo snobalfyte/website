b6b0e2e11fa565f32460c03b0d35d351dee27202
mkuba50 <mkuba50@outlook.com>
2017-10-08 01:20:03 +0200

    Add script friendly listing of ids in fileinfo database

---
+++ listid.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Copyright 2017 UUP dump authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+require_once 'api/listid.php';
+header('Content-Type: text/plain');
+
+$ids = uupListIds();
+if(isset($ids['error'])) {
+    die($ids['error']);
+}
+
+foreach($ids['builds'] as $val) {
+    echo $val['build'];
+    echo '|';
+    echo $val['arch'];
+    echo '|';
+    echo $val['uuid'];
+    echo '|';
+    echo $val['title'];
+    echo "\n";
+}
+?>
