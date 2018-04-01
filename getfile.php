dbd0dfc41b6ce2803a94ab1e9e6df77b95bc5d2d
mkuba50 <mkuba50@outlook.com>
2018-04-01 20:23:00 +0200

    Added getfile.php for easy retrieving of single files from UUP

---
+++ getfile.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Copyright 2018 UUP dump authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+$updateId = isset($_GET['id']) ? $_GET['id'] : null;
+$file = isset($_GET['file']) ? $_GET['file'] : null;
+
+if(empty($updateId)) die('Unspecified update id');
+if(empty($file)) die('Unspecified file');
+
+require_once 'api/get.php';
+require_once 'shared/get.php';
+
+$files = uupGetFiles($updateId, 0, 0);
+if(isset($files['error'])) {
+    die($files['error']);
+}
+
+$files = $files['files'];
+$filesKeys = array_keys($files);
+
+if(!isset($files[$file]['url'])) {
+    die('We couldn\'t find file '.$file);
+}
+
+$url = $files[$file]['url'];
+header('Location: '.$url);
+echo '<h1>Moved to <a href="'.$url.'">here</a>.';
+?>
--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 // Website information
-$websiteVersion = '3.9.0';
+$websiteVersion = '3.9.1';
 $requiredApi = '1.11.1';
 
 require_once dirname(__FILE__).'/../api/shared/main.php';
